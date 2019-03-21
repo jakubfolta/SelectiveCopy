adbcc310dee33277ae657cb51acef81b06962060
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-21 22:15:41 +0100

    continuation

---
+++ selective_copy.py
@@ -0,0 +1,10 @@
+#! python3
+
+# selective_copy.py - Search files with certain files extensions and copy these files to another folder.
+
+# TODO: Import essential modules.
+# TODO: Use for loop and os.listdir to find files with wanted extension.
+# TODO: Skip files without match.
+# TODO: Print result and what will be done.
+# TODO: Copy wanted files to other destination.
+# TODO: Change project status on github.
