acc5f748cccab5ad22e287183c483a2b42da4b09
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-22 13:14:06 +0100

    continuation

--- selective_copy.py
+++ selective_copy.py
@@ -2,9 +2,19 @@
 
 # selective_copy.py - Search files with certain files extensions and copy these files to another folder.
 
-# TODO: Import essential modules.
-# TODO: Use for loop and os.listdir to find files with wanted extension.
-# TODO: Skip files without match.
-# TODO: Print result and what will be done.
-# TODO: Copy wanted files to other destination.
+# Import essential modules.
+import os
+import shutil
+
+# Create variables with directory to check and directory to copies folder.
+path_to_check = 'C:\Users\ogi-8\Desktop\PythonProjects'
+copies_path = 'C:\Users\ogi-8\Desktop\PythonProjects\SelectiveCopy\TXT_files'
+# Use for loop and os.listdir to find files with wanted extension.
+for file in os.listdir(path_to_check):
+    if file.endswith('.txt'):
+# Print result and what will be done.
+        print('Copy {} to {}'.format(file, copies_path))
+# Copy wanted files to other destination.
+        #shutil.copy(file, copies_path)
+
 # TODO: Change project status on github.
