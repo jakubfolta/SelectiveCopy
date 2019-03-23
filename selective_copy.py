aa8dd7ca35e3f32ac0ac31cf9d609234fe5b113a
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-23 18:38:17 +0100

    continuation

--- selective_copy.py
+++ selective_copy.py
@@ -7,14 +7,24 @@ import os
 import shutil
 
 # Create variables with directory to check and directory to copies folder.
-path_to_check = 'C:\Users\ogi-8\Desktop\PythonProjects'
-copies_path = 'C:\Users\ogi-8\Desktop\PythonProjects\SelectiveCopy\TXT_files'
+path_to_check = 'C:\\Users\ogi-8\Desktop\PythonProjects'
+copies_path = 'C:\\Users\ogi-8\Desktop\PythonProjects\SelectiveCopy\TXT_files'
+
 # Use for loop and os.listdir to find files with wanted extension.
 for file in os.listdir(path_to_check):
-    if file.endswith('.txt'):
+    
+    :
+        file_path == os.path.abspath('..\\' + file)
+        print(file_path)
+
+# Get abs path to matched file.
+        print(file)
+        print(os.path.abspath(file))
+
 # Print result and what will be done.
         print('Copy {} to {}'.format(file, copies_path))
+
 # Copy wanted files to other destination.
-    dgfs    #shutil.copy(file, copies_path)
+        shutil.copy(file, copies_path)
 
 # TODO: Change project status on github.
